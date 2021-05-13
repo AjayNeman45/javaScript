2134649dc3a2dc103eaa48b178ac878ad41f7340
Ajay Neman <ajayneman0987&gmail.com>
2021-05-13 12:55:07 +0530

    Initial commit

---
+++ color flipper/script.js
@@ -0,0 +1,22 @@
+const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+const btn = document.getElementById("btn");
+console.log(btn);
+var color = document.querySelector(".color");
+console.log(color);
+
+btn.addEventListener("click",function(){
+    let hexColor ="#";
+    for(var i=0;i<6;i++)
+    {
+        hexColor += hex[getRandomNumber()];
+    }
+    console.log(hexColor);
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
+
